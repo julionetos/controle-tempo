444cd5f666531b671ae5eb439b752aa73bb7f0f6
julionetos <jndesignbr@gmail.com>
2026-05-08 16:49:25 -0300

    Initialize Firebase app and export auth and db

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+js
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBnPDpDOXWCz-p1AsHy4f3V_MzmOLoSJ34",
+  authDomain: "controle-de-tarefas-d6ea6.firebaseapp.com",
+  projectId: "controle-de-tarefas-d6ea6",
+  storageBucket: "controle-de-tarefas-d6ea6.firebasestorage.app",
+  messagingSenderId: "71347896162",
+  appId: "1:71347896162:web:3b5e18d0443638376fb63b"
+}
+
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
+export const db = getFirestore(app)
